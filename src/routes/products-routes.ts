9df156fefbf9fab61d3166a08701c87eb7da9934
Wallace Ribeiro <wallaceribeiro72@gmail.com>
2025-08-07 12:08:27 -0300

    create types for database

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -6,4 +6,6 @@ const productsController = new ProductController()
 
 productsRoutes.get("/", productsController.index)
 
+productsRoutes.post("/", productsController.create)
+
 export { productsRoutes }
