ee27b89a75feb088b6a9ba1ba19b4cad02e9f86d
Wallace Ribeiro <wallaceribeiro72@gmail.com>
2025-08-13 10:01:34 -0300

    create migration session tables and controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import { tablesSessionsRoutes } from "./tables-sessions-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
 
 const routes = Router()
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRoutes)
+routes.use("/tables-sessions", tablesSessionsRoutes)
 
 export { routes }
