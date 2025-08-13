5bb557ec4e4a937866a9159f0a12f2576b6b638c
Wallace Ribeiro <wallaceribeiro72@gmail.com>
2025-08-13 16:45:24 -0300

    cret

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ordersRoutes } from "./orders-routes";
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
@@ -8,5 +9,6 @@ const routes = Router()
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRoutes)
 routes.use("/tables-sessions", tablesSessionsRoutes)
+routes.use("/orders", ordersRoutes)
 
 export { routes }
